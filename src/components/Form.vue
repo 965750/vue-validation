a249427abac456914a93124c0636c8698c01c10e
Adrian Zajac <aventhelast@gmail.com>
2018-04-25 23:22:57 +0200

    validation added

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,39 +1,90 @@
 <template>
-    <div class="formCont">
+  <div>
+    <!-- Form container -->
+    <div v-show="!logged" class="formCont">
         <div class="bg"></div>
         <div><span class="formBtn formActive">Login</span><span class="formBtn">sign Up</span></div>
         <form method="POST" action="">
             <fieldset>
                 <label class="inpTitle" for="email">email</label>
-                <input class="inpPost" type="text" name="email" id="email">
+                <input class="inpPost" type="text" name="email" id="email" v-model="form.email">
                 <label class="inpTitle" for="password">password</label>
-                <input class="inpPost" type="password" name="password" id="password">
+                <input class="inpPost" type="password" name="password" id="password" v-model="form.pass">
                 <div class="checkboxCont">
                     <label class="inpTitle" for="remember">Remember me</label>
                     <div class="squaredFour">
-                        <input type="checkbox" value="None" id="remember" name="check" checked />
+                        <input type="checkbox" value="None" id="remember" name="check" />
                         <label for="remember"></label>
                     </div>
                 </div>
-                <input class="inpSubmit" type="submit" value="login">
+                <input class="inpSubmit" type="submit" value="login" v-on:click.prevent="validate">
             </fieldset>
         </form>
-        <div class="errorCont"><span class="errorText">Just a simple error</span></div>
+        <div class="errorCont"><span class="errorText">{{ error }}</span></div>
     </div>
+    <!-- Success container -->
+    <transition name="fade">
+      <div v-show="logged" class="infoCont">
+        <h1>login successful</h1>
+      </div>
+    </transition>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "app",
   data() {
     return {
-      msg: ""
+      error: "",
+      iter: 0,
+      logged: false,
+      form: {
+        email: "",
+        pass: "",
+        passRegex: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/
+      },
+      // DB
+      usersBase: {
+        email: ["test@test.pl"],
+        pass: ["Password1"]
+      }
     };
+  },
+  methods: {
+    // Are inputs correct?
+    validate() {
+        
+      if (
+        this.form.email.length < 5 ||
+        this.form.email.includes("@") == false ||
+        this.form.email.includes(".") == false
+      ) {
+        this.error = "invalid email";
+      } else if (this.form.passRegex.test(this.form.pass) == false) {
+        this.error = "invalid password";
+      } else if (this.userCheck()) {
+        this.error = "";
+        this.logged = true;
+      }
+    },
+    // Is user in DB?
+    userCheck() {
+        
+      for (this.iter; this.iter < this.usersBase.email.length; this.iter++) {
+        if(this.form.email == this.usersBase.email[this.iter] && this.form.pass == this.usersBase.pass[this.iter]){
+          return true;
+        } else {
+          this.error = "invalid email or password";
+        }
+      }
+      this.iter = '0';
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+// Form
 .formCont {
   width: 400px;
   height: 340px;
@@ -43,13 +94,13 @@ export default {
   transform: translate(-50%, -50%);
   padding: 10px;
   box-sizing: border-box;
-  background: rgba(165, 203, 69, 0.76);
+  background: rgba(141, 173, 58, 0.76);
   border-radius: 15px;
   box-shadow: 0 0 5px #333;
   .errorCont {
-    border-bottom: 2px solid #d6e7aa;
-    margin-top: 10px;
-    text-align: center;
+    border-bottom: 2px solid #fff;
+    margin-top: 0px;
+    padding-left: 20px;
     height: 20px;
     .errorText {
       color: crimson;
@@ -175,6 +226,32 @@ export default {
         }
       }
     }
+  }
+}
+// Success
+.fade-enter-active, .fade-leave-active{
+  transition: all .4s ease-out;
+}
+.fade-enter, .fade-leave-to{
+  opacity: 0;
+}
+.infoCont{
+  @extend .formCont;
+  height: 100px;
+  background: rgb(140, 173, 58);
+  h1{
+    color: #fff;
+    text-align: center;
+    margin: 0px;
+    padding: 0px;
+    line-height: 80px;
+    font-size: 25px;
+  }
+}
+// RWD
+@media screen and (max-width: 480px){
+  .formCont{
+    width: 310px;
   }
 }
 </style>
